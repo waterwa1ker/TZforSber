507054091250a1b63f3bc92ee26b082edc24cbf8
waterwa1ker <93448687+waterwa1ker@users.noreply.github.com>
2023-05-30 17:51:59 +0300

    Authorization added

---
+++ src/main/java/com/TaskForSber/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.TaskForSber.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public ResponseEntity<HttpStatus> getStatus(){
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+}
