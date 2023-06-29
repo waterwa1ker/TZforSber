4cb34c169dd4cfd8b38873ff50f72b007f48d87c
waterwa1ker <93448687+waterwa1ker@users.noreply.github.com>
2023-06-29 22:51:39 +0300

    second version

--- src/main/java/com/TaskForSber/exceptions/book/BookNotCreatedException.java
+++
@@ -1,7 +0,0 @@
-package com.TaskForSber.exceptions.book;
-
-public class BookNotCreatedException extends RuntimeException {
-    public BookNotCreatedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/TaskForSber/exceptions/user/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.TaskForSber.exceptions.user;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
